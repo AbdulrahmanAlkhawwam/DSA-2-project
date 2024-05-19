72694b737e0e88fe1195565459d61a2b92f4d1cf
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2024-05-19 10:53:41 +0300

    create main master

---
+++ src/BinaryTreeNode.java
@@ -0,0 +1,11 @@
+public class BinaryTreeNode {
+    char data;
+    BinaryTreeNode left;
+    BinaryTreeNode right;
+
+    public BinaryTreeNode(char data) {
+        this.data = data;
+        this.left= null;
+        this.right = null;
+    }
+}
